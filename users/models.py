4af522fe1e082b9a5e50875ca62eec6262e308b1
Vladislav <talcore@yandex.ru>
2025-06-15 23:29:39 +0500

    feat: update Patiend, Doctor models and serializers

--- users/models.py
+++ users/models.py
@@ -2,23 +2,13 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class Role(models.Model):
-    ROLE_CHOICES = (
-        (1, 'Пациент'),
-        (2, 'Доктор'),
-        (3, 'Администратор'),
-    )
-
-    id = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, primary_key=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        verbose_name = "Роль"
-        verbose_name_plural = "Роли"
-
 class User(AbstractUser):
-    role_id = models.ForeignKey(Role, on_delete=models.SET_NULL, null=True, blank=False, )
+    ROLE_CHOICES = (
+        (1, "Patient"),
+        (2, "Doctor"),
+        (3, "Admin"),
+    )
+    role = models.IntegerField(choices=ROLE_CHOICES, null=True, blank=False)
 
 
 class Patient(User):
--- users/serializers.py
+++ users/serializers.py
@@ -1,26 +1,44 @@
 from rest_framework import serializers
-from .models import Role, Doctor, Patient
-
-
-class RoleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Role
-        fields = ['id']
+from clinics.models import Clinic
+from .models import Doctor, Patient
 
 
 class PatientSerializer(serializers.ModelSerializer):
-    role_id = RoleSerializer(many=True, read_only=True)
-
     class Meta:
         model = Patient
-        fields = ['id', 'first_name', 'role_id', 'last_name', 'patronymic', 'username', 'password', 'email']
+        fields = ('id', 'first_name', 'role', 'phone', 'last_name', 'patronymic', 'username', 'password', 'email')
+
+    def create(self, validated_data):
+        artist = Patient.objects.create(**validated_data)
+        artist.save()
+
+        return artist
 
 
 class DoctorSerializer(serializers.ModelSerializer):
-    role_id = RoleSerializer(many=True, read_only=True)
+    clinic = serializers.PrimaryKeyRelatedField(queryset=Clinic.objects.all())
+
+    def create(self, validated_data):
+        clinic = Clinic.objects.filter(name=validated_data["clinic"]).first()
+        validated_data.pop("clinic")
+        doctor = Doctor.objects.create(**validated_data)
+        clinic.doctor_set.add(doctor)
+
+        doctor.save()
+
+        return doctor
 
     class Meta:
         model = Doctor
-        fields = ['id', 'first_name', 'last_name', 'patronymic', 'speciality', 'created_at', 'updated_at']
-
-
+        fields = (
+            'id',
+            'username',
+            'first_name',
+            'role',
+            'clinic',
+            'last_name',
+            'patronymic',
+            'speciality',
+            'created_at',
+            'updated_at'
+        )
